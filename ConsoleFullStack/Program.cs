c3b5d8bfc3042e28d3bdd93642b0e5df80b5803d
fraze-dev <aaron.fraze3@gmail.com>
2026-08-24 11:03:00 -0400

    Initial commit

---
+++ ConsoleFullStack/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankingApp;
+
+class Program{
+
+    static void Main(){
+        WelcomeMessage();
+
+        Console.ReadKey();
+    }
+
+    static void WelcomeMessage(){
+        Console.WriteLine("Welcome to Banking Application");
+    }
+
+}
