40d0e8840625bc7f9722c3b4bd45448810c999c0
fraze-dev <aaron.fraze3@gmail.com>
2026-08-24 13:29:44 -0400

    Add: class to allow user types created

---
+++ ConsoleFullStack/Admin.cs
@@ -0,0 +1,11 @@
+namespace BankingApp;
+
+public class Admin : User
+{
+    public Admin(string username, string password)
+        : base(username, password)
+    {
+    }
+
+    public override string GetRole() => "Admin";
+}
---
+++ ConsoleFullStack/Customer.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace BankingApp;
+
+public class Customer : User
+{
+    private List<Account> accounts;
+
+    // Overloaded constructors: start with no accounts, or hand in an existing list
+    public Customer(string username, string password)
+        : this(username, password, new List<Account>())
+    {
+    }
+
+    public Customer(string username, string password, List<Account> accounts)
+        : base(username, password)
+    {
+        this.accounts = accounts;
+    }
+
+    public List<Account> Accounts => accounts;
+
+    public void AddAccount(Account account)
+    {
+        accounts.Add(account);
+    }
+
+    public override string GetRole() => "Customer";
+}
